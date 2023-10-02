f44371868e3f1125b25fd024bf28434136f20a66
Dogu <altindoguhan@gmail.com>
2023-10-02 18:12:31 +0300

    Shipment Size

--- src/main/java/com/trendyol/shipment/Basket.java
+++ src/main/java/com/trendyol/shipment/Basket.java
@@ -1,13 +1,37 @@
 package com.trendyol.shipment;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Basket {
 
     private List<Product> products;
+    private Map<ShipmentSize,Long> getProductCountbyShipmentSize() {
+
+        return products.stream().collect(Collectors.groupingBy(e -> e.getSize(), Collectors.counting()));
+    }
+
+    private Map.Entry<ShipmentSize, Long> getMaxItemSizeinBasket(Map<ShipmentSize,Long> ProductCountbyShipmentSize) {
+    //Map<ShipmentSize,Long> ProductCountbyShipmentSize = getProductCountbyShipmentSize();
+
+    return ProductCountbyShipmentSize.entrySet().stream().max((entry1, entry2) -> entry1.getValue() > entry2.getValue() ? 1 : -1).get();
+    }
+    private  ShipmentSize getLargestItemSizeinBasket( Map<ShipmentSize,Long> ProductCountbyShipmentSize) {
+        //Map<ShipmentSize,Long> ProductCountbyShipmentSize = getProductCountbyShipmentSize();
+        return  ProductCountbyShipmentSize.entrySet().stream()
+                .max(Map.Entry.comparingByKey()).get().getKey();
+    }
+
+
 
     public ShipmentSize getShipmentSize() {
-        return null;
+        Map<ShipmentSize,Long> productCountbyShipmentSize=  getProductCountbyShipmentSize();
+        Map.Entry<ShipmentSize, Long> MaxItemSizeinBasket = getMaxItemSizeinBasket(productCountbyShipmentSize);
+        if(MaxItemSizeinBasket.getValue()>=3){
+            return MaxItemSizeinBasket.getKey().nextSize();
+        }
+        return getLargestItemSizeinBasket(productCountbyShipmentSize);
     }
 
     public List<Product> getProducts() {
@@ -18,3 +42,5 @@ public class Basket {
         this.products = products;
     }
 }
+//2 Small _> Medium
+//2 Medium --> Large
--- src/main/java/com/trendyol/shipment/ShipmentSize.java
+++ src/main/java/com/trendyol/shipment/ShipmentSize.java
@@ -1,9 +1,21 @@
 package com.trendyol.shipment;
 
+import java.util.List;
+
 public enum ShipmentSize {
 
     SMALL,
     MEDIUM,
     LARGE,
-    X_LARGE
+    X_LARGE;
+    private static final ShipmentSize[] values = ShipmentSize.values();
+    public ShipmentSize nextSize() {
+        int ordinal = this.ordinal();
+        if (ordinal < values.length - 1) {
+            return values[ordinal + 1];
+        } else {
+            return this;
+        }
+    }
+
 }
